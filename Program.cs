a57b42e9af3f637507868bba67f9239a0a7a4d7f
FelipeSoouza <146121300+FelipeSoouza@users.noreply.github.com>
2024-04-15 11:11:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CadastroCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de Cliente");
+            Console.WriteLine();
+
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("CPF: ");
+            string cpf = Console.ReadLine();
+
+            Console.Write("Telefone: ");
+            string telefone = Console.ReadLine();
+
+            Console.Write("Endereço: ");
+            string endereco = Console.ReadLine();
+
+            Console.Write("Estado Civil: ");
+            string estadoCivil = Console.ReadLine();
+
+            Console.Write("Nome da mãe: ");
+            string nomeMae = Console.ReadLine();
+
+            Console.Write("Nome do pai: ");
+            string nomePai = Console.ReadLine();
+
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Profissão: ");
+            string profissao = Console.ReadLine();
+
+            Console.Write("Gênero: ");
+            string genero = Console.ReadLine();
+
+            Console.Write("Nacionalidade: ");
+            string nacionalidade = Console.ReadLine();
+
+            Console.Write("Data de Nascimento (DD/MM/AAAA): ");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Altura (em metros): ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Peso (em kg): ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Tipo Sanguíneo: ");
+            string tipoSanguineo = Console.ReadLine();
+
+
+
+            Console.WriteLine("----agora voce está cadastrado----");
+        }
+    }
+}
+
